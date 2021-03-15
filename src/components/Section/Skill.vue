bfa8159ee4c3d747998c29669906f0b423111178
vasin sermsampan <dev.vasin@gmail.com>
2021-03-15 21:38:52 +0700

    update text

--- src/components/Section/Skill.vue
+++ src/components/Section/Skill.vue
@@ -108,7 +108,7 @@ export default {
             'network technology',
             'service and infrastructure',
             'software development',
-            'storage and data'
+            'IT storage and data'
           ]
         },
         {
